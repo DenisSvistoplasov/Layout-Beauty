eca424885c86cd1bcdcca932dcb7e47f87d9fa04
Denis <deniska_roms@mail.ru>
2023-02-21 20:40:43 +0500

    added section quality

---
+++ js/script.js
@@ -0,0 +1,32 @@
+(() => {
+
+  new Swiper('.quality-slider', {
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.quality-slider__button_next',
+      prevEl: '.quality-slider__button_prev',
+    },
+    
+    // Pagination
+    breakpoints: {
+      320: {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
+      },
+      680: {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'custom',
+          renderCustom: function(_, i) {
+            return (i + '').length == 1 ? '0' + i : i;
+          }
+        },
+      },
+    }
+  });
+  
+})();
